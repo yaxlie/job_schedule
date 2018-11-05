5b4232c39cd2b723d2f8d65156ba1527018e15e1
mlesnier <m.lesnierowski@gmail.com>
2018-11-05 18:08:12 +0100

    Fix sort

--- src/Scheduler.py
+++ src/Scheduler.py
@@ -8,30 +8,65 @@ def schedule(problem, h = 0.8, mode = "default"):
     result = []
     for instance in problem:
         sch_time = 0.0 # czas algorytmu
-        t = 0 # aktualny czas
         F = 0 # funkcja kosztu
+        r = 0 # przesunięcie
         SUM_P = sum(job.time for job in instance) # suma czasów zadań
         d = math.floor(SUM_P * h)  # moment rozpoczęcia
         if mode == "sort":
-            inst_temp = []
+            # wstępne sortowanie
+            instance = sortJobs(instance, d)
+
+            # manipulacja czasem startu
             t_temp = 0
-            instance = sorted(instance, key=lambda job: job.c2)
+            p = 0.1  # procent zadań na krawędziach (czas wykonania)
+            left = [] # zadania na lewej krawędzi
+            right = [] # zadanie na prawej krawędzi
+
             for job in instance:
-                if t_temp < d:
-                    t_temp += job.time
-                    inst_temp.append(job)
-                    instance.remove(job)
-            instance = sorted(instance, key=lambda job: job.c2, reverse=True)
-            instance = inst_temp + instance
+                if (t_temp + job.time < p * SUM_P) or (len(left) == 0):
+                    left.append(job)
+
+            for job in reversed(instance):
+                if (t_temp + job.time < p * SUM_P) or (len(right) == 0):
+                    right.append(job)
+
+            r = (sum(job.c1 for job in left) - sum(job.c2 for job in right)) / (sum(job.c1 for job in left) + sum(job.c2 for job in right))
+            # print("SUM_P={} \t r={}".format(SUM_P, r))
+            r = 0 if r < 0 else int(r/2 * SUM_P)
+
+            # ostateczne sortowanie
+            instance = sortJobs(instance, d)
+
+        t = r
         for job in instance:
-            if t < d:
-                F += job.c1 * math.fabs(d - t)
             t += job.time
+            if t <= d:
+                F += job.c1 * math.fabs(d - t)
+                # print("before {} {} {}".format(job.c1, job.c2, math.fabs(d - t)))
             if t > d:
                 F += job.c2 * math.fabs(d - t)
-
+                # print("after {} {} {}".format(job.c1, job.c2, math.fabs(d - t)))
+        # print("\n")
             # print("id={} time={} c1={} c2={}".format(job.id, job.time, job.c1, job.c2))
-        result.append((F, h, d, sch_time))
+        result.append((F, h, d, sch_time, instance))
     return result
+
+def sortJobs(instance, d):
+    inst_temp = []
+    inst_temp2 = []
+    t_temp = 0
+    # print(d)
+    instance = sorted(instance, key=lambda job: job.c1)
+    for job in instance:
+        t_temp += job.time
+        if t_temp < d:
+            # print("b {}".format(t_temp))
+            inst_temp.append(job)
+        else:
+            # print("a {}".format(t_temp))
+            inst_temp2.append(job)
+        inst_temp2 = sorted(inst_temp2, key=lambda job: job.c2, reverse=True)
+    instance = inst_temp + inst_temp2
+    return instance
 
 
--- src/main.py
+++ src/main.py
@@ -10,13 +10,14 @@ def main():
         for h in H:
             print("\n{} h={}".format(file.title(), h))
             problem = FileLoader.load(os.path.join(path, file))
-            result0 = Scheduler.schedule(problem, h, "default")
             t0 = time()
             result = Scheduler.schedule(problem, h, "sort")
             t1 = time()
             for k in range(0, len(result)):
                 # print("\n{} {} {} [id ' ']\n".format(r[0], r[1], r[2]))
-                print("{} {} {} {}% {}s".format(k, result0[k][0], result[k][0], int(result0[k][0] / result[k][0] * 100), float("{0:.3f}".format(t1 - t0))))
+                print("{} {} {}s".format(k, result[k][0], float("{0:.3f}".format(t1 - t0))))
+                # for job in result[k][4]:
+                #     print("(id={} c1={} c2={} time={})".format(job.id, job.c1, job.c2, job.time))
         print("\n")
 
 main()
